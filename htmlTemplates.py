94e877d477fca6355125fa9fc2889d473e176272
IFTrejoM <71332826+IFTrejoM@users.noreply.github.com>
2023-08-02 09:44:21 -0500

    Updates and dependencies fixed

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="images/bot_image.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="C:\\Users\\Iván Trejo\\OneDrive\\DEP\\talk_to_pdf\\ivan_trejo.jpg">
+        <img src="images/user_image_2.jpg">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
